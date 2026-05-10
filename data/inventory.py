252409941a6f15c0fc7d971e614fa144071d36e9
klyde <klyde0929@gmail.com>
2026-05-11 01:59:19 +0800

    final

--- data/inventory.py
+++
@@ -1,1 +0,0 @@
-inventory = {'Burger': {'price': 59.0, 'stock': 190}, 'Fries': {'price': 35.0, 'stock': 100}, 'Chicken': {'price': 99.0, 'stock': 100}, 'Soda': {'price': 25.0, 'stock': 100}, 'Hotdog': {'price': 40.0, 'stock': 100}, 'Ice Cream': {'price': 35.0, 'stock': 100}, 'Extra Gravy': {'price': 15.0, 'stock': 100}}
